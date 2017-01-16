1ccf5eaf199d0a13671636df383b81c21d0b96ec
Calvin92 <calvin1992.js@gmail.com>
2017-01-16 16:59:59 +0800

    add project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <QRCode val="https://baidu.com" :show="true" />
+  </div>
+</template>
+
+<script>
+import QRCode from './qrcode.vue'
+export default {
+  name: 'app',
+  components: {
+    QRCode
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
